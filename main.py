fdcac0531ba708f3a06bc615efc9cc04e8230dce
Will Farris <9190155+WillFarris@users.noreply.github.com>
2021-12-07 00:30:01 -0800

    normalize data between 0 and 1

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 # Train/test data is stored in a tuple of (data, labels)
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 
+# Scale data to range [0, 1)
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
 # Translations from numeric label (index in 'labels' array) to English item name
 labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
