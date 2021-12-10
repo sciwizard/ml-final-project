474bef1895d3e5e4ffdd105eaece9b08628ded84
sciwizard <audio.sauce@gmail.com>
2021-12-09 21:26:02 -0800

    added accuracy comparison, plotting with matplotlib

--- cnn.py
+++ cnn.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-def cnn_classifier(train_data, test_data, labels, batch_size=100):
+def cnn_classifier(train_data, test_data, labels, batch_size=100, split=0.2):
     (x_train, y_train) = train_data
     (x_test, y_test) = test_data
 
@@ -21,7 +21,7 @@ def cnn_classifier(train_data, test_data, labels, batch_size=100):
     x_train = x_train.reshape(x_train.shape[0], *image_shape)
     x_test = x_test.reshape(x_test.shape[0], *image_shape)
 
-    x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+    x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size=split, random_state=42)
 
     x_validate = x_validate.reshape(x_validate.shape[0], *image_shape)
 
@@ -31,16 +31,16 @@ def cnn_classifier(train_data, test_data, labels, batch_size=100):
 
 
     model= Sequential([
-    Conv2D(filters=32, kernel_size=3, activation='sigmoid'),
+    Conv2D(filters=32, kernel_size=3, activation='tanh'),
     MaxPooling2D(pool_size=2),
     Dropout(0.2),
     Flatten(),
-    Dense(32, activation='sigmoid'),
-    Dense(32, activation='sigmoid'),
+    Dense(32, activation='tanh'),
+    Dense(32, activation='tanh'),
     Dense(10, activation='softmax')])
 
     tensor_board = TensorBoard(
-        log_dir = r'logs/{}'.format('layers_2_lr_0_1_sigmoid'),
+        log_dir = r'logs/{}'.format('layers_2_lr_0_1_compare'),
         write_graph=True,
         write_grads = True,
         histogram_freq = 1,
@@ -63,7 +63,9 @@ def cnn_classifier(train_data, test_data, labels, batch_size=100):
                 verbose = 1, validation_data=(x_validate, y_validate),
                 callbacks = tensor_board)
 
-    evalulation_score = model.evaluate(x_test, y_test, verbose = 0)
+    evalulation_score = model.evaluate(x_validate, y_validate, verbose = 0)
 
     print('    test loss : {:.4f}'.format(evalulation_score[0]))
     print('test accuracy : {:.4f}'.format(evalulation_score[1]))
+
+    return evalulation_score[1]
--- main.py
+++ main.py
@@ -10,11 +10,24 @@ from cnn import cnn_classifier
 
 def main():
     if len(sys.argv) < 3:
-        print("usage: python main.py <CNN/SVM> <SINGLE/FULL>")
+        print("usage: python main.py <CNN/SVM/COMP> <SINGLE/FULL>")
         exit(-1)
 
-    is_svm = True if sys.argv[1].upper() == "SVM" else False
-    full_svm_test = True if sys.argv[2].upper() == "FULL" else False
+    is_svm = False
+    is_cnn = False
+    compare = False
+    full_svm_test = False
+
+    arg1 = sys.argv[1].upper()
+    arg2 = sys.argv[2].upper()
+    if arg1 == "SVM":
+        is_svm = True
+    elif arg1 == "COMP":
+        compare = True
+    elif arg1 == "CNN":
+        is_cnn = True
+    if arg2 == "FULL":
+        full_svm_test = True
 
     # Train/test data is stored in a tuple of (data, labels)
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
@@ -56,9 +69,27 @@ def main():
         plt.legend()
         plt.show()
 
-    else:
+    if is_cnn:
         # for i in range(1):
         cnn_classifier((x_train, y_train), (x_test, y_test), labels)
+
+    if compare:
+        svm_accuracy = []
+        cnn_accuracy = []
+        x_values = [6000, 12000, 24000, 36000, 48000]
+        for sample_size in x_values:
+            svm_accuracy += svm_classifier((x_train, y_train), (x_test, y_test), labels, kernel='rbf',
+                                           num_samples=sample_size)
+        for split_size in (.9, .8, .6, .4, .2):
+            cnn_accuracy += cnn_classifier((x_train, y_train), (x_test, y_test), labels, split=split_size)
+
+        plt.title('Best CNN vs SVN Accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('# training samples')
+        plt.plot(x_values, svm_accuracy, label='SVN')
+        plt.plot(x_values, cnn_accuracy, label='CNN')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
